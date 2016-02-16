cc30332b330efc913db2b0ee6f631198613cc516
Jayashree Nagarajan <jayashree.nagarajan@daqri.com>
2016-02-16 00:33:19 -0800

    Added SharedActionProvider to share Web Url as text message

--- NYTimesSearch/app/src/main/java/project/codepath/nytimessearch/activities/ArticleDetails.java
+++ NYTimesSearch/app/src/main/java/project/codepath/nytimessearch/activities/ArticleDetails.java
@@ -1,17 +1,21 @@
 package project.codepath.nytimessearch.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import project.codepath.nytimessearch.R;
 
 public class ArticleDetails extends AppCompatActivity {
+
+    ShareActionProvider miShareAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,4 +42,23 @@ public class ArticleDetails extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_details, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        // get reference to WebView
+        WebView wvArticle = (WebView) findViewById(R.id.wvArticle);
+        // pass in the URL currently being used by the WebView
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+
+        miShareAction.setShareIntent(shareIntent);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
